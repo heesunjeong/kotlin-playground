6b9f38d19abf2c3cfd837ee28f32072e45eb81a6
heesunjeong <jhswit@naver.com>
2020-11-05 22:01:46 +0900

    add assertj dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 }
 dependencies {
     testImplementation(kotlin("test-junit5"))
+    testImplementation("org.assertj:assertj-core:3.11.1")
 }
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
